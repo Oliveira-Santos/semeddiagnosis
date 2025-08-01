ba6dc8670c87ddc5bbbe793962fc4ffa611afab4
SemedCanaaDosCarajas <oliveirasantostecnology@gmail.com>
2025-07-31 21:56:30 -0300

    Sistema atualizado em 31 de julho de 2025 as 21:56

--- webproject/settings.py
+++ webproject/settings.py
@@ -130,13 +130,13 @@ LOGIN_REDIRECT_URL = '/dashboard/'  # Substitua pelo caminho correto da sua dash
 
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "shelton.oliveira.barbosa@gmail.com"  # Substitua pelo seu e-mail
-EMAIL_HOST_PASSWORD = "nhiq aagp vxvx fzlo"  # Use a senha de aplicativo do Gmail
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "shelton.oliveira.barbosa@gmail.com"  
+# EMAIL_HOST_PASSWORD = "nhiq aagp vxvx fzlo"  
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
